8ff0e5ff7f9cb2d8f524adb7ea0709fdde294929
T Chandirasekar <chandru9279@gmail.com>
2011-09-25 15:31:17 +0530

    chandru: improved performance with spiral room

--- java/src/zasz/me/Disorganizer.scala
+++ java/src/zasz/me/Disorganizer.scala
@@ -18,8 +18,8 @@ import java.awt._
 class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Width: Int, var Height: Int)
 {
   var Tags = InTags.asScala.map(x => (x._1, Int.unbox(x._2)))
-  val _Increment: (Double) => Double = (X: Double) => X + 1
-  val _Decrement: (Double) => Double = (X: Double) => X - 1
+  var _Increment: (Double) => Double = (X: Double) => X + _Room
+  var _Decrement: (Double) => Double = (X: Double) => X - _Room
   val _Die = (Msg: String) => throw new Exception(Msg)
 
   if (null == Tags || 0 == Tags.size)
@@ -30,7 +30,7 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
   private val _MaxEdgeSize: Int = if (Width >= Height) Width else Height;
   private val _MainArea: Rectangle2D.Double = new Rectangle2D.Double(0, 0, Width, Height)
   private val _Center = new Point2D.Double(Width / 2f, Height / 2f);
-  private val _SpiralEndSentinel: Point2D.Double = new Point2D.Double(_MaxEdgeSize + 10, _MaxEdgeSize + 10);
+  private val _SpiralEndSentinel: Point2D.Double = new Point2D.Double(_MaxEdgeSize + 100, _MaxEdgeSize + 100);
   private val _TagsSorted = ListMap[String, Int](Tags.toList.sortBy[Int](_._2): _*)
   private val _HighestWeight: Int = _TagsSorted.head._2
   private val _LowestWeight: Int = _TagsSorted.last._2
@@ -44,68 +44,81 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
   private var _CurrentEdgeSize: Int = 0
   private var _SleepingEdge: Boolean = false
   private var _ServiceObjectNew: Boolean = true
+  private var _Room: Int = 1
 
-  /// <summary>
-  ///   Default is Times New Roman
-  /// </summary>
+  /**
+   * Default is 0. Do not go beyond 20.
+   * This determines performance : higher = faster.
+   * A higher number means a more roomy spiral will place words
+   */
+  @BeanProperty var SpiralRoom: Int = 0
+
+  private def oddSpiralRoom(): Int =
+  {
+    2 * SpiralRoom + 1
+  }
+
+  /**
+   *   Default is Times New Roman
+   */
   @BeanProperty var SelectedFont: Font = new Font("Times New Roman", 0, 15)
 
-  /// <summary>
-  ///   Default is false, Enable to start seeing Word boundaries used for
-  ///   collision detection.
-  /// </summary>
+  /**
+   *   Default is false, Enable to start seeing Word boundaries used for
+   *   collision detection.
+   */
   @BeanProperty var ShowWordBoundaries: Boolean = false
 
-  /// <summary>
-  ///   Set this to true, if vertical must needs to appear with RHS as floor
-  ///   Default is LHS is the floor and RHS is ceiling of the Text.
-  /// </summary>
+  /**
+   *   Set this to true, if vertical must needs to appear with RHS as floor
+   *   Default is LHS is the floor and RHS is ceiling of the Text.
+   */
   @BeanProperty var VerticalTextRight: Boolean = true
 
-  /// <summary>
-  ///   Size of the smallest String in the TagCloud
-  /// </summary>
+  /**
+   *   Size of the smallest String in the TagCloud
+   */
   @BeanProperty var MinimumFontSize: Float = 12f
 
-  /// <summary>
-  ///   Size of the largest String in the TagCloud
-  /// </summary>
+  /**
+   *   Size of the largest String in the TagCloud
+   */
   @BeanProperty var MaximumFontSize: Float = 82f
 
-  /// <summary>
-  ///   Use <code>DisplayStrategy.Get()</code> to get a Display Strategy
-  ///   Default is RandomHorizontalOrVertical.
-  /// </summary>
+  /**
+   *   Use <code>DisplayStrategy.Get()</code> to get a Display Strategy
+   *   Default is RandomHorizontalOrVertical.
+   */
   @BeanProperty var DisplayChoice: DisplayStrategy = DisplayStrategy.Get(TagDisplayStrategy.AllVertical)
 
-  /// <summary>
-  ///   Use <code>ColorStrategy.Get()</code> to get a Color Strategy
-  ///   Default is white background and random darker foreground colors.
-  /// </summary>
+  /**
+   *   Use <code>ColorStrategy.Get()</code> to get a Color Strategy
+   *   Default is white background and random darker foreground colors.
+   */
   @BeanProperty var ColorChoice: ColorStrategy = ColorStrategy.Get(Theme.LightBgDarkFg, Style.RandomVaried, Color.WHITE, Color.BLACK)
 
-  /// <summary>
-  ///   A rotate transform will be applied on the whole image based on this
-  ///   Angle in degrees. Which means the Boundaries are not usable for hover animations
-  ///   in CSS/HTML.
-  /// </summary>
+  /**
+   *   A rotate transform will be applied on the whole image based on this
+   *   Angle in degrees. Which means the Boundaries are not usable for hover animations
+   *   in CSS/HTML.
+   */
   @BeanProperty var Angle: Int = 0
 
-  /// <summary>
-  ///   Default is false. Set this to true to crop out blank background.
-  /// </summary>
+  /**
+   *   Default is false. Set this to true to crop out blank background.
+   */
   @BeanProperty var Crop: Boolean = false
 
-  /// <summary>
-  ///   Default is 30px.
-  /// </summary>
+  /**
+   *   Default is 30px.
+   */
   @BeanProperty var Margin: Double = 30f
 
-  /// <summary>
-  ///   Words that were not rendered because of non-availability
-  ///   of free area to render them. If count is anything other than 0
-  ///   use a bigger bitmap as input with more area.
-  /// </summary>
+  /**
+   *   Words that were not rendered because of non-availability
+   *   of free area to render them. If count is anything other than 0
+   *   use a bigger bitmap as input with more area.
+   */
   @BeanProperty var WordsSkipped: HashMap[String, Int] = new HashMap[String, Int];
 
 
@@ -121,6 +134,7 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
   {
     if (_ServiceObjectNew) _ServiceObjectNew = false
     else _Die("This object has been used. Dispose this, create and use a new Service object.")
+    _Room = oddSpiralRoom()
     val cloudImage = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_ARGB)
     val GImage: Graphics2D = cloudImage.createGraphics()
     GImage.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
@@ -171,7 +185,7 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
 
   def CalculateWhere(Measure: Rectangle2D.Double): Point2D.Double =
   {
-    _CurrentEdgeSize = 1;
+    _CurrentEdgeSize = _Room;
     _SleepingEdge = true;
     _CurrentCorner = _Center;
 
@@ -189,7 +203,7 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
   }
 
   /*
-   * This method gives poIntegers that crawls along an edge of the spiral, described below.
+   * This method gives points that crawls along an edge of the spiral, described below.
    */
   def GetNextPointInEdge(Cur: Point2D.Double): Point2D.Double =
   {
@@ -208,18 +222,18 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
     Current
   }
 
-  /* Imagine a grid of 5x5 poIntegers, and 0,0 and 4,4 are the topright and bottomleft respectively.
+  /* Imagine a grid of 5x5 points, and 0,0 and 4,4 are the topright and bottomleft respectively.
    * You can move in a spiral by navigating as follows:
-   * 1. Inc GivenPoInteger's X by 1 and return it.
-   * 2. Inc GivenPoInteger's Y by 1 and return it.
-   * 3. Dec GivenPoInteger's X by 2 and return it.
-   * 4. Dec GivenPoInteger's Y by 2 and return it.
-   * 5. Inc GivenPoInteger's X by 3 and return it.
-   * 6. Inc GivenPoInteger's Y by 3 and return it.
-   * 7. Dec GivenPoInteger's X by 4 and return it.
-   * 8. Dec GivenPoInteger's Y by 4 and return it.
+   * 1. Inc Givenpoint's X by 1 and return it.
+   * 2. Inc Givenpoint's Y by 1 and return it.
+   * 3. Dec Givenpoint's X by 2 and return it.
+   * 4. Dec Givenpoint's Y by 2 and return it.
+   * 5. Inc Givenpoint's X by 3 and return it.
+   * 6. Inc Givenpoint's Y by 3 and return it.
+   * 7. Dec Givenpoint's X by 4 and return it.
+   * 8. Dec Givenpoint's Y by 4 and return it.
    *
-   * I'm calling the values 1,2,3,4 etc as _EdgeSize. Any joining of poIntegers in a graph is an edge.
+   * I'm calling the values 1,2,3,4 etc as _EdgeSize. Any joining of points in a graph is an edge.
    * To find out if we need to increment or decrement I'm using the condition _EdgeSize is even or not.
    * To increment EdgeSize, using a booleanean _SleepingEdge to count upto 2 steps. I'll blog about this later
    * at chandruon.net!
@@ -248,14 +262,14 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
     else
     {
       Y = if (EdgeSizeEven) PreviousCorner.y - _CurrentEdgeSize else PreviousCorner.y + _CurrentEdgeSize;
-      _CurrentEdgeSize = _CurrentEdgeSize + 1;
+      _CurrentEdgeSize = _CurrentEdgeSize + _Room;
       _SleepingEdge = true;
     }
 
     _EdgeDirection = if (EdgeSizeEven) _Decrement else _Increment
 
-    /* If the spiral widens to a poInteger where its arms are longer than the Height & Width,
-    * it's time to end the spiral and give up placing the word. There is no 'poInteger'
+    /* If the spiral widens to a point where its arms are longer than the Height & Width,
+    * it's time to end the spiral and give up placing the word. There is no 'point'
     * (no pun Integerended) in going for wider spirals, as you are out of bounds now.
     * Our spiral is an Archimedean Right spiral, made up of Line segments @
     * right-angles to each other.
@@ -274,13 +288,13 @@ class Disorganizer(var InTags: java.util.Map[String, java.lang.Integer], var Wid
     (WeightScaled * _FontHeightSpan) + MinimumFontSize
   }
 
-  /// <summary>
-  ///   Uses the list of occupied areas to
-  ///   crop the Bitmap and translates the list of occupied areas
-  ///   keeping them consistant with the new cropped bitmap
-  /// </summary>
-  /// <param name = "CloudToCrop">The bitmap of the cloud to crop</param>
-  /// <returns>The cropped version of the bitmap</returns>
+  /**
+   *   Uses the list of occupied areas to
+   *   crop the Bitmap and translates the list of occupied areas
+   *   keeping them consistant with the new cropped bitmap
+   *   CloudToCrop : The bitmap of the cloud to crop
+   *   returns : The cropped version of the bitmap
+   */
   def CropAndTranslate(CloudToCrop: BufferedImage): BufferedImage =
   {
     var NewTop = _Occupied.map(x => scala.Double.box(x.y)).min(Ordering[Double]) - Margin
